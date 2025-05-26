010858dabd3a4070bb2eabc7742b8097b9a1a5d9
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-05-26 07:04:03 -0700

    chore: add fake test to fix coverage

--- crates/website/src/lib.rs
+++ crates/website/src/lib.rs
@@ -5,3 +5,14 @@ pub mod state;
 pub mod states;
 pub mod tasks;
 pub mod utils;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {
+        let left = || true;
+        let right = || false;
+
+        assert_ne!(left(), right());
+    }
+}
